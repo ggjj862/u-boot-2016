4ff774cb9f3fcfc626790e70fb64a8ab4af71cd6
ggjj862 <54201549+ggjj862@users.noreply.github.com>
2026-04-15 19:26:57 +0800

    Update main.c
    Signed-off-by: ggjj862 <54201549+ggjj862@users.noreply.github.com>

--- common/main.c
+++ common/main.c
@@ -62,43 +62,47 @@ static void run_preboot_environment_command(void)
 /* We come here after U-Boot is initialised and ready to process commands */
 void main_loop(void)
 {
-	const char *s = NULL;
+    const char *s = NULL;
 
-	bootstage_mark_name(BOOTSTAGE_ID_MAIN_LOOP, "main_loop");
+    bootstage_mark_name(BOOTSTAGE_ID_MAIN_LOOP, "main_loop");
 
 #ifndef CONFIG_SYS_GENERIC_BOARD
-	puts("Warning: Your board does not use generic board. Please read\n");
-	puts("doc/README.generic-board and take action. Boards not\n");
-	puts("upgraded by the late 2014 may break or be removed.\n");
+    puts("Warning: Your board does not use generic board. Please read\n");
+    puts("doc/README.generic-board and take action. Boards not\n");
+    puts("upgraded by the late 2014 may break or be removed.\n");
 #endif
 
 #ifndef CONFIG_REDUCE_FOOTPRINT
-	modem_init();
+    modem_init();
 #ifdef CONFIG_VERSION_VARIABLE
-	setenv("ver", version_string);  /* set version variable */
-#endif /* CONFIG_VERSION_VARIABLE */
+    setenv("ver", version_string);
+#endif
 #endif
 
-	cli_init();
+    cli_init();
 
 #ifndef CONFIG_REDUCE_FOOTPRINT
-	run_preboot_environment_command();
+    run_preboot_environment_command();
 #endif
 
 #if defined(CONFIG_UPDATE_TFTP)
-	update_tftp(0UL, NULL, NULL);
-#endif /* CONFIG_UPDATE_TFTP */
+    update_tftp(0UL, NULL, NULL);
+#endif
 
 #ifdef CONFIG_HTTPD
-	check_button_is_press();
+    check_button_is_press();
 #endif
-	s = bootdelay_process();
+    s = bootdelay_process();
+
+    /* 强制使用网络启动命令，忽略原有的 bootcmd */
+    s = "setenv serverip 192.168.1.10; setenv bootfile openwrt.itb; tftpboot; bootm 0x44000000";
+
 #ifndef CONFIG_REDUCE_FOOTPRINT
-	if (cli_process_fdt(&s))
-		cli_secure_boot_cmd(s);
+    if (cli_process_fdt(&s))
+        cli_secure_boot_cmd(s);
 #endif
 
-	autoboot_command(s);
+    autoboot_command(s);
 
-	cli_loop();
+    cli_loop();
 }
